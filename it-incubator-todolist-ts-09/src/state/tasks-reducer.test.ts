0429e26c6960b29db73f0fffbf6aed270b4801aa
fantazer21 <6967221@gmail.com>
2021-05-14 08:21:27 +0200

    'update

--- it-incubator-todolist-ts-09/src/state/tasks-reducer.test.ts
+++ it-incubator-todolist-ts-09/src/state/tasks-reducer.test.ts
@@ -15,9 +15,8 @@ test('Correct REMOVE_TASK', () => {
         ]
     };
 
-    const action = tasksReducer("2", "todolistId2");
-
-    const endState = tasksReducer(startState, action)
+    const endState = tasksReducer(startState, {type: 'REMOVE_TASK',todolistId: 'todolistId2', taskId: '2'
+         })
 
     expect(endState).toEqual({
         "todolistId1": [
@@ -33,28 +32,69 @@ test('Correct REMOVE_TASK', () => {
 
 });
 
+test('Correct ADD_TASK', () => {
+    const startState: TasksStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+    const newTask = { id: "4", title: "Juice", isDone: false }
 
-// test('correct task should be added to correct array', () => {
-//     const startState: TasksStateType = {
-//         "todolistId1": [
-//             { id: "1", title: "CSS", isDone: false },
-//             { id: "2", title: "JS", isDone: true },
-//             { id: "3", title: "React", isDone: false }
-//         ],
-//         "todolistId2": [
-//             { id: "1", title: "bread", isDone: false },
-//             { id: "2", title: "milk", isDone: true },
-//             { id: "3", title: "tea", isDone: false }
-//         ]
-//     };
-//
-//     const action = addTaskAC("juce", "todolistId2");
-//
-//     const endState = tasksReducer(startState, action)
-//
-//     expect(endState["todolistId1"].length).toBe(3);
-//     expect(endState["todolistId2"].length).toBe(4);
-//     expect(endState["todolistId2"][0].id).toBeDefined();
-//     expect(endState["todolistId2"][0].title).toBe("juce");
-//     expect(endState["todolistId2"][0].isDone).toBe(false);
-// })
+
+
+    const endState = tasksReducer(startState, {type:'ADD_TASK', todolistId: 'todolistId2', newTask: newTask,})
+
+    expect(endState["todolistId1"].length).toBe(3);
+    expect(endState["todolistId2"].length).toBe(4);
+    expect(endState["todolistId1"][2].title).toBe('React');
+    expect(endState["todolistId2"][3].id).toBe('4');
+    expect(endState["todolistId2"][3].title).toBe("Juice");
+    expect(endState["todolistId2"][3].isDone).toBe(false);
+})
+
+test( 'Correct CHANGE_TASK_STATUS', ()=> {
+        const startState: TasksStateType = {
+            "todolistId1": [
+                { id: "1", title: "CSS", isDone: false },
+                { id: "2", title: "JS", isDone: true },
+                { id: "3", title: "React", isDone: false }
+            ],
+            "todolistId2": [
+                { id: "1", title: "bread", isDone: false },
+                { id: "2", title: "milk", isDone: true },
+                { id: "3", title: "tea", isDone: false }
+            ]
+        };
+
+        const endState = tasksReducer(startState, {type: 'CHANGE_TASK_STATUS', todolistId: 'todolistId2', taskId: '2', isDone: false})
+
+        expect(endState[ "todolistId2"][1].title).toBe('milk');
+        expect(endState[ "todolistId2"][1].isDone).toBe(false)
+})
+
+test( 'Correct CHANGE_TASK_TITLE', ()=> {
+    const startState: TasksStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+
+    const endState = tasksReducer(startState, {type: 'CHANGE_TASK_TITLE', todolistId: 'todolistId2', taskId: '2', title: 'Coffee'})
+
+    expect(endState[ "todolistId2"][1].title).toBe('Coffee');
+    expect(endState[ "todolistId2"][1].isDone).toBe(true)
+})
--- it-incubator-todolist-ts-09/src/state/tasks-reducer.ts
+++ it-incubator-todolist-ts-09/src/state/tasks-reducer.ts
@@ -1,35 +1,37 @@
 import {FilterValuesType, TodolistType} from '../App';
 import {v1} from 'uuid';
 import {TasksStateType} from '../App';
-import {TaskType} from "../Todolist";
+
 
 export type RemoveTaskActionType = {
     type: 'REMOVE_TASK'
     taskId: string
     todolistId: string
-
 }
+
 export type AddTaskActionType = {
     type: 'ADD_TASK'
-    taskId: string
     todolistId: string
+    newTask: {id: string, title: string, isDone: boolean}
 
 }
-export type ChangeTaskActionType = {
-    type: 'CHANGE_TASK'
-    taskId: string
+
+export type ChangeTaskStatusActionType = {
+    type: 'CHANGE_TASK_STATUS'
+    isDone: boolean
     todolistId: string
+    taskId: string
 
 }
 
 export type ChangeTaskTitleActionType = {
     type: 'CHANGE_TASK_TITLE'
     todolistId: string,
+    taskId: string
     title: string
 }
 
-
-type ActionsType = RemoveTaskActionType | AddTaskActionType | ChangeTaskActionType | ChangeTaskTitleActionType
+type ActionsType = RemoveTaskActionType | AddTaskActionType | ChangeTaskStatusActionType | ChangeTaskTitleActionType
 
 export const tasksReducer = (TasksState: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
@@ -37,65 +39,34 @@ export const tasksReducer = (TasksState: TasksStateType, action: ActionsType): T
             let copyState = {...TasksState}
             copyState[action.todolistId] = copyState[action.todolistId].filter(task => task.id !== action.taskId)
             return copyState
-        // case 'ADD_TASK':
-        //     // let newTask: TaskType = {id: v1(), title: action.title, isDone: false}
-        //     // let newTasksTodolist = [newTask, ...state[action.todolistId]]
-        //     // return {
-        //     //     ...state,
-        //     //     [action.todolistId]: newTasksTodolist
-        //     // }
-        //
-        // case 'CHANGE_TASK':
-        //
-        // case 'CHANGE_TASK_TITLE':
 
+        case 'ADD_TASK':
+            let newTasksTodolist = [ ...TasksState[action.todolistId], action.newTask]
+            return {
+                ...TasksState,
+                [action.todolistId]: newTasksTodolist
+            }
+
+        case 'CHANGE_TASK_STATUS':
+        let case2 = {...TasksState}
+    // найдём нужную таску:
+    let task = case2[action.todolistId].find(t => t.id === action.taskId);
+    //изменим таску, если она нашлась
+    if (task) {
+        task.isDone = action.isDone;
+        // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
+
+    }
+    return {...case2};
+
+         case 'CHANGE_TASK_TITLE':
+             let case4 = {...TasksState}
+             let task4 = case4[action.todolistId].find(t => t.id === action.taskId);
+             if (task4) {
+                 task4.title = action.title;// засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
+             }
+             return {...case4};
         default:
-            return state
+            throw new Error('Bla Bla Bla')
     }
 }
-
-//
-// function removeTask(id: string, todolistId: string) {
-//     //достанем нужный массив по todolistId:
-//     let todolistTasks = tasks[todolistId];
-//     // перезапишем в этом объекте массив для нужного тудулиста отфилтрованным массивом:
-//     tasks[todolistId] = todolistTasks.filter(t => t.id != id);
-//     // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-//     setTasks({...tasks});
-// }
-//
-// function addTask(title: string, todolistId: string) {
-//     let task = {id: v1(), title: title, isDone: false};
-//     //достанем нужный массив по todolistId:
-//     let todolistTasks = tasks[todolistId];
-//     // перезапишем в этом объекте массив для нужного тудулиста копией, добавив в начало новую таску:
-//     tasks[todolistId] = [task, ...todolistTasks];
-//     // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-//     setTasks({...tasks});
-// }
-//
-// function changeStatus(id: string, isDone: boolean, todolistId: string) {
-//     //достанем нужный массив по todolistId:
-//     let todolistTasks = tasks[todolistId];
-//     // найдём нужную таску:
-//     let task = todolistTasks.find(t => t.id === id);
-//     //изменим таску, если она нашлась
-//     if (task) {
-//         task.isDone = isDone;
-//         // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-//         setTasks({...tasks});
-//     }
-// }
-//
-// function changeTaskTitle(id: string, newTitle: string, todolistId: string) {
-//     //достанем нужный массив по todolistId:
-//     let todolistTasks = tasks[todolistId];
-//     // найдём нужную таску:
-//     let task = todolistTasks.find(t => t.id === id);
-//     //изменим таску, если она нашлась
-//     if (task) {
-//         task.title = newTitle;
-//         // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-//         setTasks({...tasks});
-//     }
-// }
